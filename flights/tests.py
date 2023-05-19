f5e210315fcc25594860bdd40e7becf77ee6b873
alexndra1272 <alexandrag1272@gmail.com>
2023-05-19 11:23:36 -0600

    A

--- flights/tests.py
+++ flights/tests.py
@@ -23,12 +23,13 @@ class FlightsTestCase(TestCase):
     def test_arrivals_count(self):
         a = Airport.objects.get(code="AAA")
         self.assertEqual(a.arrivals.count(), 1)
-
+    """
     def test_valid_flight(self):
         a1 = Airport.objects.get(code="AAA")
         a2 = Airport.objects.get(code="BBB")
         f = Flight.objects.get(origin=a1, destination=a2)
         self.assertTrue(f.is_valid_flight())
+    """
 
     def test_invalid_flight_destination(self):
         a1 = Airport.objects.get(code="AAA")
